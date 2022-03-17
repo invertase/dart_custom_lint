c500ee0b84fff8c0aac49ccba3ffc6f89463eb56
Remi Rousselet <darky12s@gmail.com>
2022-03-17 14:47:25 +0100

    Add hot-restart support

--- packages/custom_lint/lib/src/analyzer_plugin/plugin_link.dart
+++ packages/custom_lint/lib/src/analyzer_plugin/plugin_link.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
+import 'dart:io';
 import 'dart:isolate';
 
+import 'package:async/async.dart' show StreamGroup;
 import 'package:path/path.dart' as p;
 import 'package:analyzer/file_system/file_system.dart' as analyzer;
 import 'package:analyzer_plugin/protocol/protocol.dart' as plugin;
@@ -8,119 +10,140 @@ import 'package:analyzer_plugin/protocol/protocol_common.dart' as plugin;
 import 'package:analyzer_plugin/protocol/protocol_generated.dart' as plugin;
 import 'package:analyzer_plugin/src/protocol/protocol_internal.dart' as plugin
     show RequestParams;
+import 'package:riverpod/riverpod.dart';
 
 import '../../protocol.dart';
 import '../log.dart';
+
+final _pluginSourceChangeProvider =
+    StreamProvider.autoDispose.family<void, Uri>((ref, pluginRootUri) {
+  final pluginRootPath = pluginRootUri.toFilePath();
+
+  return StreamGroup.merge([
+    Directory(p.join(pluginRootPath, 'lib')).watch(recursive: true),
+    Directory(p.join(pluginRootPath, 'bin')).watch(recursive: true),
+    // watch package dir but not recursively, for pubspec/analysis changes
+    File(p.join(pluginRootPath, 'pubspec.yaml')).watch(recursive: true),
+    File(
+      p.join(pluginRootPath, '.dart_tool', 'package_config.json'),
+    ).watch(recursive: true),
+    // TODO possibly watch package dependencies too, for when working on custom_lint
+  ]);
+});
+
+final pluginLinkProvider =
+    Provider.autoDispose.family<PluginLink, Uri>((ref, pluginRootUri) {
+  log('build plugin $pluginRootUri');
+  ref.watch(_pluginSourceChangeProvider(pluginRootUri));
+  ref.listen<Object?>(_pluginSourceChangeProvider(pluginRootUri), (_, value) {
+    log('Source changed for $pluginRootUri: $value');
+  });
+
+  ref.onDispose(() {
+    log('Close plugin $pluginRootUri');
+  });
+
+  final receivePort = ReceivePort();
+  ref.onDispose(receivePort.close);
+
+  final pluginRootPath = pluginRootUri.toFilePath();
+  // TODO configure that through build.yaml-like file
+  final mainPath = Uri.file(
+    p.join(pluginRootPath, 'lib', 'main.dart'),
+  );
+
+  final isolate = Isolate.spawnUri(
+    mainPath,
+    const [],
+    receivePort.sendPort,
+    automaticPackageResolution: true,
+  );
+
+// // TODO do we ca re about killing isolates before _listenIsolate completes?
+
+  final sendPortCompleter = Completer<SendPort>();
+
+  final link = PluginLink._(
+    isolate,
+    sendPortCompleter.future,
+    pluginRootUri,
+  );
+  ref.onDispose(link.close);
+
+  // TODO close subscribption
+  receivePort.listen(
+    (Object? obj) {
+      if (obj is SendPort) {
+        sendPortCompleter.complete(obj);
+        return;
+      }
+
+      // log('Received $obj');
+
+      try {
+        final json = Map<String, Object?>.from(obj! as Map);
+
+        if (json.containsKey(plugin.Notification.EVENT)) {
+          final notification = plugin.Notification.fromJson(json);
+
+          switch (json[plugin.Notification.EVENT]) {
+            case PrintParams.key:
+              final print = PrintParams.fromNotification(notification);
+              link._messagesController.add(print);
+              break;
+            case 'plugin.error':
+              final error =
+                  plugin.PluginErrorParams.fromNotification(notification);
+              link._errorsController.add(error);
+              break;
+            default:
+              link._notificationsController.add(notification);
+          }
+        } else {
+          final response = plugin.Response.fromJson(json);
+          link._responsesController.add(response);
+        }
+      } catch (err, stack) {
+        log('failed to decode message $obj with:\n$err\n$stack');
+        // TODO handle
+      }
+    },
+    // TODO handle errors
+    onDone: () {
+      link._errorsController.close();
+      link._messagesController.close();
+      link._responsesController.close();
+      link._notificationsController.close();
+    },
+  );
+
+  return link;
+});
 
 class PluginLink {
   PluginLink._(
     this._isolate,
     this._sendPort,
-    this._responsesController,
-    this._notificationsController,
-    this._messagesController,
-    this._errorController,
-    this._receivePort,
     this.key,
   );
-
-  factory PluginLink.spawn(Uri pluginRootUri) {
-    // TODO configure that through build.yaml-like file
-    final mainPath = Uri.file(
-      p.join(pluginRootUri.toFilePath(), 'lib', 'main.dart'),
-    );
-
-    final receivePort = ReceivePort();
-
-    final isolate = Isolate.spawnUri(
-      mainPath,
-      const [],
-      receivePort.sendPort,
-      automaticPackageResolution: true,
-    );
-
-// // TODO do we ca re about killing isolates before _listenIsolate completes?
-
-    final errors = StreamController<plugin.PluginErrorParams>.broadcast();
-    final messages = StreamController<PrintParams>.broadcast();
-    final responses = StreamController<plugin.Response>.broadcast();
-    final notifications = StreamController<plugin.Notification>.broadcast();
-    final sendPortCompleter = Completer<SendPort>();
-
-    // TODO close subscribption
-    receivePort.listen(
-      (Object? obj) {
-        if (obj is SendPort) {
-          sendPortCompleter.complete(obj);
-          return;
-        }
-
-        try {
-          final json = Map<String, Object?>.from(obj! as Map);
-
-          if (json.containsKey(plugin.Notification.EVENT)) {
-            final notification = plugin.Notification.fromJson(json);
-
-            switch (json[plugin.Notification.EVENT]) {
-              case PrintParams.key:
-                final print = PrintParams.fromNotification(notification);
-                messages.add(print);
-                break;
-              case 'plugin.error':
-                final error =
-                    plugin.PluginErrorParams.fromNotification(notification);
-                errors.add(error);
-                break;
-              default:
-                notifications.add(notification);
-            }
-          } else {
-            final response = plugin.Response.fromJson(json);
-            responses.add(response);
-          }
-        } catch (err, stack) {
-          log('failed to decode message $obj with:\n$err\n$stack');
-          // TODO handle
-        }
-      },
-      // TODO handle errors
-      onDone: () {
-        errors.close();
-        messages.close();
-        responses.close();
-        notifications.close();
-      },
-    );
-
-    return PluginLink._(
-      isolate,
-      sendPortCompleter.future,
-      responses,
-      notifications,
-      messages,
-      errors,
-      receivePort,
-      pluginRootUri,
-    );
-  }
 
   final Uri key;
   final Future<Isolate> _isolate;
   final Future<SendPort> _sendPort;
-  final ReceivePort _receivePort;
-  final contextRoots = <plugin.ContextRoot>{};
   final lintsForLibrary = <String, plugin.AnalysisErrorsParams>{};
 
-  final StreamController<PrintParams> _messagesController;
+  final _messagesController = StreamController<PrintParams>.broadcast();
   Stream<PrintParams> get messages => _messagesController.stream;
 
-  final StreamController<plugin.PluginErrorParams> _errorController;
-  Stream<plugin.PluginErrorParams> get error => _errorController.stream;
+  final _errorsController =
+      StreamController<plugin.PluginErrorParams>.broadcast();
+  Stream<plugin.PluginErrorParams> get error => _errorsController.stream;
 
-  final StreamController<plugin.Response> _responsesController;
+  final _responsesController = StreamController<plugin.Response>.broadcast();
   Stream<plugin.Response> get responses => _responsesController.stream;
 
-  final StreamController<plugin.Notification> _notificationsController;
+  final _notificationsController =
+      StreamController<plugin.Notification>.broadcast();
   Stream<plugin.Notification> get notifications =>
       _notificationsController.stream;
 
@@ -129,11 +152,12 @@ class PluginLink {
   }
 
   Future<void> close() async {
-    _receivePort.close();
-    _messagesController.close();
-    _errorController.close();
-    _notificationsController.close();
-    _responsesController.close();
-    return _isolate.then((i) => i.kill());
+    await Future.wait<void>([
+      _isolate.then((value) => value.kill()),
+      _errorsController.close(),
+      _responsesController.close(),
+      _messagesController.close(),
+      _notificationsController.close(),
+    ]);
   }
 }
--- packages/custom_lint/tools/analyzer_plugin/bin/plugin.dart
+++ packages/custom_lint/tools/analyzer_plugin/bin/plugin.dart
@@ -2,10 +2,6 @@ import 'dart:isolate';
 
 import 'package:custom_lint/src/analyzer_plugin/analyzer_plugin_starter.dart';
 
-import 'log.dart';
-
 void main(List<String> args, SendPort sendPort) {
-  log('main');
   start(args, sendPort);
-  // Future<void>.delayed(Duration(hours: 2));
 }
