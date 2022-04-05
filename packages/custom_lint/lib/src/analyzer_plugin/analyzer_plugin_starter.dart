f53bd55c7253de339f33d28168059eb0f3de58b2
Remi Rousselet <darky12s@gmail.com>
2022-04-05 11:47:34 +0200

    Fix warnings

--- packages/custom_lint/lib/src/analyzer_plugin/analyzer_plugin_starter.dart
+++ packages/custom_lint/lib/src/analyzer_plugin/analyzer_plugin_starter.dart
@@ -1,21 +1,26 @@
 import 'dart:async';
 import 'dart:isolate';
 
-import '../log.dart';
+import 'package:analyzer_plugin/protocol/protocol_generated.dart';
+
 import 'analyzer_plugin.dart';
 import 'client_isolate_channel.dart';
 
 /// Connects custom_lint to the analyzer server using the analyzer_plugin protocol
 void start(Iterable<String> _, SendPort sendPort) {
+  final channel = ClientIsolateChannel(sendPort);
+
   runZonedGuarded(
     () {
-      final channel = ClientIsolateChannel(sendPort);
       final server = CustomLintPlugin();
-      log('start server ${server.name}');
       server.start(channel);
     },
     (err, stack) {
-      log('Uncaught error:\n$err\n$stack');
+      sendPort.send(
+        PluginErrorParams(false, err.toString(), stack.toString())
+            .toNotification()
+            .toJson(),
+      );
     },
   );
 }
