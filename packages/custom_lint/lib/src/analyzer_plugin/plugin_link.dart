4023fe1927422d115891614f1612405a1bb307f0
Remi Rousselet <darky12s@gmail.com>
2022-11-16 14:46:44 +0100

    Fix compiation error

--- packages/custom_lint/lib/src/analyzer_plugin/plugin_link.dart
+++ packages/custom_lint/lib/src/analyzer_plugin/plugin_link.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 import 'dart:isolate';
 
 import 'package:analyzer_plugin/protocol/protocol_generated.dart' as plugin;
-import 'package:async/async.dart';
+import 'package:async/async.dart' show StreamGroup;
 import 'package:package_config/package_config.dart';
 import 'package:path/path.dart' as p;
 import 'package:pubspec_parse/pubspec_parse.dart';
