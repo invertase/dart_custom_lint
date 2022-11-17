5d0ca23b3cac784312984990304defcc20079fa2
Remi Rousselet <darky12s@gmail.com>
2022-11-17 11:45:46 +0100

    Disable hot-restart if hot-reload is enabled

--- packages/custom_lint/lib/src/analyzer_plugin/plugin_link.dart
+++ packages/custom_lint/lib/src/analyzer_plugin/plugin_link.dart
@@ -36,6 +36,12 @@ class PluginKey {
 
 final _pluginSourceChangeProvider =
     StreamProvider.autoDispose.family<void, PluginKey>((ref, pluginRootUri) {
+  if (ref.watch(watchModeProvider)) {
+    // Watch mode already takes care of reloading plugins. So doing a hot-restart
+    // on source change would only slow down hot-reload.
+    return const Stream.empty();
+  }
+
   ref.cache5();
   final pluginRootPath = pluginRootUri.uri.toFilePath();
 
