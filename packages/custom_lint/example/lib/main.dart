95c91c9ba0ab816615f47b78b955ae08dbc67458
Remi Rousselet <darky12s@gmail.com>
2023-01-05 20:00:28 +0100

    Apply suggestions from code review

--- packages/custom_lint/example/lib/main.dart
+++ packages/custom_lint/example/lib/main.dart
@@ -9,7 +9,7 @@ class Main {}
 // expect_lint: riverpod_final_provider
 ProviderBase<int> provider = Provider((ref) => 0);
 
-// expect_lints: riverpod_final_provider
+// expect_lint: riverpod_final_provider
 Provider<int> provider2 = Provider((ref) => 0);
 
 Object? foo = 42;
