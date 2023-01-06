68d81ec684a621456e311fc2bd480c6d707e8d8b
Remi Rousselet <darky12s@gmail.com>
2023-01-06 08:38:37 +0100

    Add assist support
    closes #15

--- packages/custom_lint/example/example_lint/lib/custom_lint_example_lint.dart
+++ packages/custom_lint/example/example_lint/lib/custom_lint_example_lint.dart
@@ -1,6 +1,8 @@
 import 'package:analyzer/dart/analysis/results.dart';
 import 'package:analyzer/dart/element/element.dart';
 import 'package:analyzer/dart/element/type.dart';
+import 'package:analyzer_plugin/protocol/protocol_generated.dart';
+import 'package:analyzer_plugin/utilities/change_builder/change_builder_core.dart';
 import 'package:custom_lint_builder/custom_lint_builder.dart';
 
 bool _isProvider(DartType type) {
@@ -37,7 +39,48 @@ class _RiverpodLint extends PluginBase {
         code: 'riverpod_final_provider',
         message: 'Providers should always be declared as final',
         location: provider.nameLintLocation!,
+        getAnalysisErrorFixes: (lint) async* {
+          final changeBuilder = ChangeBuilder(
+            session: resolvedUnitResult.session,
+          );
+          await changeBuilder.addDartFileEdit(resolvedUnitResult.path,
+              (builder) {
+            builder.addInsertion(lint.location.offset, (builder) {
+              builder.write('final');
+            });
+          });
+          final sourceChange = changeBuilder.sourceChange
+            ..message = 'Make final';
+
+          yield AnalysisErrorFixes(lint.asAnalysisError(), fixes: [
+            PrioritizedSourceChange(1, sourceChange),
+          ]);
+        },
       );
     }
+  }
+
+  @override
+  Future<EditGetAssistsResult> handleGetAssists(
+    ResolvedUnitResult resolvedUnitResult, {
+    required int offset,
+    required int length,
+  }) async {
+    final changeBuilder = ChangeBuilder(
+      session: resolvedUnitResult.session,
+    );
+    await changeBuilder.addDartFileEdit(resolvedUnitResult.path, (builder) {
+      builder.addInsertion(offset, (builder) {
+        builder.write('HelloWorld');
+      });
+    });
+
+    final sourceChange = changeBuilder.sourceChange
+      ..message = 'Prefix with HelloWorld';
+    // print(sourceChange.toJson());
+
+    return EditGetAssistsResult([
+      PrioritizedSourceChange(1, sourceChange),
+    ]);
   }
 }
