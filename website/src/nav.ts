275978a58d1a8c31ce06c6e2f3a782af7be3176a
Eugene Lokotkov <webdevleo@gmail.com>
2023-02-09 19:16:12 +0200

    chore(website): add website homepage (#73)
    * Add page skeleton
    
    * Update Dark Mode UI. Add fonts. Finish header UI for desktop view
    
    * Update mobile UI
    
    * Add mobile menu
    
    * Add dark theme backgrounds
    
    * Finish dark theme UI
    
    * Cleanup meta tags
    
    * Remove unused variable
    
    * chore: ready to publish
    
    * chore: add more plugins
    
    ---------
    
    Co-authored-by: Eugene Lokotkov <eugene.lokotkov@geniussports.com>
    Co-authored-by: Majid <mhadaily@gmail.com>

---
+++ website/src/nav.ts
@@ -0,0 +1,10 @@
+export default [
+  {
+    href: 'https://docs.custom-lint.dev',
+    title: 'Docs',
+  },
+  {
+    href: 'https://github.com/invertase/dart_custom_lint',
+    title: 'Github',
+  },
+];
