2e28c1bb5197d06166d38196fae2915e66bb7bcb
Remi Rousselet <darky12s@gmail.com>
2023-02-13 17:16:27 +0100

    Fix `TypeChecker.fromPackage` not always return `true` when it should

--- packages/custom_lint_core/test/type_checker_test.dart
+++ packages/custom_lint_core/test/type_checker_test.dart
@@ -1,8 +1,11 @@
+import 'dart:io';
+
 import 'package:analyzer/dart/analysis/results.dart';
 import 'package:analyzer/dart/analysis/utilities.dart';
 import 'package:analyzer/dart/ast/ast.dart';
 import 'package:analyzer/dart/ast/visitor.dart';
 import 'package:custom_lint_core/custom_lint_core.dart';
+import 'package:path/path.dart';
 import 'package:test/test.dart';
 
 import 'assist_test.dart';
@@ -29,6 +32,69 @@ void main() {
         );
       }),
     );
+  });
+
+  group('TypeChecker.fromPackage', () {
+    test('matches a type from a package', () async {
+      final tempDir = Directory.systemTemp.createTempSync();
+      addTearDown(() => tempDir.deleteSync(recursive: true));
+
+      final file = File(join(tempDir.path, 'lib', 'main.dart'))
+        ..createSync(recursive: true)
+        ..writeAsStringSync('''
+class Foo {}
+int a;
+''');
+
+      final pubspec = File(join(tempDir.path, 'pubspec.yaml'));
+      pubspec.writeAsStringSync('''
+name: some_package
+version: 0.2.8
+description: A package to help writing custom linters
+repository: https://github.com/invertase/dart_custom_lint
+environment:
+  sdk: ">=2.17.1 <3.0.0"
+''');
+
+      await Process.run(
+        'dart',
+        ['pub', 'get', '--offline'],
+        workingDirectory: tempDir.path,
+      );
+
+      final unit = await resolveFile2(path: file.path);
+      unit as ResolvedUnitResult;
+
+      const checker = TypeChecker.fromPackage('some_package');
+      const checker2 = TypeChecker.fromPackage('some_package2');
+
+      expect(
+        checker.isExactlyType(
+          (unit.unit.declarations.first as ClassDeclaration)
+              .declaredElement!
+              .thisType,
+        ),
+        true,
+      );
+      expect(
+        checker2.isExactlyType(
+          (unit.unit.declarations.first as ClassDeclaration)
+              .declaredElement!
+              .thisType,
+        ),
+        false,
+      );
+
+      expect(
+        checker.isExactlyType(
+          (unit.unit.declarations[1] as TopLevelVariableDeclaration)
+              .variables
+              .type!
+              .type!,
+        ),
+        false,
+      );
+    });
   });
 }
 
