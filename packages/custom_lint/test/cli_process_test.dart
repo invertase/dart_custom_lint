979841bbc8e59bc7b981328ba1acd52f74b4c531
Peter Leibiger <kuhnroyal@gmail.com>
2023-09-01 15:19:09 +0200

    Add & improve process tests for watch mode

--- packages/custom_lint/test/cli_process_test.dart
+++ packages/custom_lint/test/cli_process_test.dart
@@ -4,6 +4,7 @@ import 'dart:io';
 
 import 'package:custom_lint/src/package_utils.dart';
 import 'package:test/test.dart';
+import 'package:test_process/test_process.dart';
 
 import 'cli_test.dart';
 import 'create_project.dart';
@@ -275,7 +276,7 @@ So, because custom_lint_client depends on test_lint from path, version solving f
       test('with exit code 0 when no lints', () async {
         final workspace = createTemporaryDirectory();
 
-        final process = await Process.start(
+        final process = await TestProcess.start(
           'dart',
           [
             customLintBinPath,
@@ -284,14 +285,14 @@ So, because custom_lint_client depends on test_lint from path, version solving f
           workingDirectory: workspace.path,
         );
 
-        expect(
-          process.stdout.map(utf8.decode),
-          emitsThrough(contains('q: Quit')),
-        );
+        // Ignore first lines
+        await process.stdout.skip(4);
+        expect(await process.stdout.next, 'q: Quit');
 
         process.stdin.write('q');
 
-        await expectLater(process.exitCode, completion(0));
+        await expectLater(process.stdout.rest, emitsThrough(emitsDone));
+        await process.shouldExit(0);
       });
 
       test('with exit code 1 when there are lints', () async {
@@ -303,8 +304,6 @@ So, because custom_lint_client depends on test_lint from path, version solving f
             TestLintRule(
               code: 'hello_world',
               message: 'Hello world',
-              onRun:
-                  r"print('${context.pubspec.name} ${context.pubspec.dependencies.keys}');",
             ),
           ]),
         );
@@ -316,7 +315,7 @@ So, because custom_lint_client depends on test_lint from path, version solving f
           name: 'test_app',
         );
 
-        final process = await Process.start(
+        final process = await TestProcess.start(
           'dart',
           [
             customLintBinPath,
@@ -325,14 +324,98 @@ So, because custom_lint_client depends on test_lint from path, version solving f
           workingDirectory: workspace.path,
         );
 
+        // Ignore first lines
+        await process.stdout.skip(6);
+        expect(await process.stdout.next, 'q: Quit');
+
+        process.stdin.write('q');
+
+        await expectLater(process.stdout.rest, emitsThrough(emitsDone));
+        await process.shouldExit(1);
+      });
+    });
+
+    group('[r] reloads', () {
+      test('with no lints', () async {
+        final workspace = createTemporaryDirectory();
+
+        final process = await TestProcess.start(
+          'dart',
+          [
+            customLintBinPath,
+            '--watch',
+          ],
+          workingDirectory: workspace.path,
+        );
+
+        // Ignore first lines
+        expect(await process.stdout.next, 'No issues found!');
+        await process.stdout.skip(3);
+        expect(await process.stdout.next, 'q: Quit');
+
+        process.stdin.write('r');
+
+        // Skip empty lines
+        await process.stdout.skip(2);
+        expect(await process.stdout.next, 'Manual Reload...');
+        expect(await process.stdout.next, 'No issues found!');
+
+        process.stdin.write('q');
+
+        await process.shouldExit(0);
+      });
+
+      test('with lints', () async {
+        final workspace = createTemporaryDirectory();
+
+        final plugin = createPlugin(
+          name: 'test_lint',
+          main: createPluginSource([
+            TestLintRule(
+              code: 'hello_world',
+              message: 'Hello world',
+            ),
+          ]),
+        );
+
+        createLintUsage(
+          parent: workspace,
+          source: {'lib/main.dart': 'void fn() {}'},
+          plugins: {'test_lint': plugin.uri},
+          name: 'test_app',
+        );
+
+        final process = await TestProcess.start(
+          'dart',
+          [
+            customLintBinPath,
+            '--watch',
+          ],
+          workingDirectory: workspace.path,
+        );
+
+        // Ignore first lines
+        await process.stdout.skip(2);
         expect(
-          process.stdout.map(utf8.decode),
-          emitsThrough(contains('q: Quit')),
+          await process.stdout.next,
+          '  test_app/lib/main.dart:1:6 • Hello world • hello_world • INFO',
+        );
+        await process.stdout.skip(3);
+        expect(await process.stdout.next, 'q: Quit');
+
+        process.stdin.write('r');
+
+        // Skip empty lines
+        await process.stdout.skip(2);
+        expect(await process.stdout.next, 'Manual Reload...');
+        expect(
+          await process.stdout.next,
+          '  test_app/lib/main.dart:1:6 • Hello world • hello_world • INFO',
         );
 
         process.stdin.write('q');
 
-        await expectLater(process.exitCode, completion(1));
+        await process.shouldExit(1);
       });
     });
   });
