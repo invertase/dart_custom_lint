aa5f31cb0c40fc4835807047c232eac779f18bc5
Daniil Marchenko <145997968+solid-daniilmarchenko@users.noreply.github.com>
2024-01-09 14:30:40 +0200

    Add Support for 'package:' configs (#205)

--- packages/custom_lint_core/test/configs_test.dart
+++ packages/custom_lint_core/test/configs_test.dart
@@ -2,7 +2,10 @@ import 'dart:io' as io;
 
 import 'package:analyzer/file_system/file_system.dart';
 import 'package:analyzer/file_system/physical_file_system.dart';
+// ignore: implementation_imports
+import 'package:custom_lint/src/package_utils.dart' show parsePackageConfig;
 import 'package:custom_lint_core/custom_lint_core.dart';
+import 'package:package_config/package_config.dart';
 import 'package:path/path.dart';
 import 'package:test/test.dart';
 
@@ -19,9 +22,54 @@ File createAnalysisOptions(String content) {
   return PhysicalResourceProvider.INSTANCE.getFile(ioFile.path);
 }
 
-void main() {
+Future<String> createTempProject(String projectName, String tempDirPath) async {
+  final projectPath = join(tempDirPath, projectName);
+
+  final dir = io.Directory(projectPath);
+  await dir.create();
+
+  final libPath = join(dir.path, 'lib');
+  await io.Directory(libPath).create();
+
+  final analysisOptionsPath = join(libPath, 'analysis_options.yaml');
+  await io.File(analysisOptionsPath).writeAsString('''
+custom_lint:
+  rules:
+    - from_package
+  ''');
+
+  return dir.path;
+}
+
+PackageConfig patchPackageConfig(
+  PackageConfig currentPackageConfig,
+  String packageName,
+  String packagePath,
+) {
+  final patchedPackages = currentPackageConfig.packages.toList()
+    ..add(
+      Package(
+        packageName,
+        Uri.file('$packagePath/'),
+        packageUriRoot: Uri.parse('lib/'),
+      ),
+    );
+
+  final patchedPackageConfig = PackageConfig(
+    patchedPackages,
+    extraData: currentPackageConfig.extraData,
+  );
+
+  return patchedPackageConfig;
+}
+
+void main() async {
   late File includeFile;
   late CustomLintConfigs includeConfig;
+
+  final packageConfig = await parsePackageConfig(io.Directory.current);
+
+  const testPackageName = 'test_package_with_config';
   setUp(() {
     includeFile = createAnalysisOptions(
       '''
@@ -35,7 +83,7 @@ custom_lint:
 ''',
     );
 
-    includeConfig = CustomLintConfigs.parse(includeFile);
+    includeConfig = CustomLintConfigs.parse(includeFile, packageConfig);
   });
 
   test('Empty config', () {
@@ -45,13 +93,14 @@ custom_lint:
 
   group('parse', () {
     test('if file is null, defaults to empty', () {
-      final configs = CustomLintConfigs.parse(null);
+      final configs = CustomLintConfigs.parse(null, packageConfig);
       expect(configs, same(CustomLintConfigs.empty));
     });
 
     test('if file does not exist, defaults to empty ', () {
       final configs = CustomLintConfigs.parse(
         PhysicalResourceProvider.INSTANCE.getFile('/this-does-no-exist'),
+        packageConfig,
       );
       expect(configs, same(CustomLintConfigs.empty));
     });
@@ -63,7 +112,7 @@ linter:
   rules:
     public_member_api_docs: false
 ''');
-      final configs = CustomLintConfigs.parse(analysisOptions);
+      final configs = CustomLintConfigs.parse(analysisOptions, packageConfig);
 
       expect(configs, includeConfig);
     });
@@ -75,7 +124,7 @@ linter:
   rules:
     public_member_api_docs: false
 ''');
-      final configs = CustomLintConfigs.parse(analysisOptions);
+      final configs = CustomLintConfigs.parse(analysisOptions, packageConfig);
 
       expect(configs, includeConfig);
     });
@@ -89,7 +138,7 @@ linter:
 
 custom_lint:
 ''');
-      final configs = CustomLintConfigs.parse(analysisOptions);
+      final configs = CustomLintConfigs.parse(analysisOptions, packageConfig);
 
       expect(configs, includeConfig);
     });
@@ -100,7 +149,7 @@ custom_lint:
   rules: 
   - a
 ''');
-      final configs = CustomLintConfigs.parse(analysisOptions);
+      final configs = CustomLintConfigs.parse(analysisOptions, packageConfig);
 
       expect(
         configs.rules,
@@ -125,7 +174,7 @@ linter:
 custom_lint:
   enable_all_lint_rules: false
 ''');
-      final configs = CustomLintConfigs.parse(analysisOptions);
+      final configs = CustomLintConfigs.parse(analysisOptions, packageConfig);
 
       expect(configs.enableAllLintRules, false);
       expect(configs.rules, includeConfig.rules);
@@ -144,12 +193,68 @@ custom_lint:
   rules:
   - a
 ''');
-      final configs = CustomLintConfigs.parse(analysisOptions);
+      final configs = CustomLintConfigs.parse(analysisOptions, packageConfig);
 
       expect(configs.enableAllLintRules, false);
       expect(configs.rules, {
         'a': const LintOptions.empty(enabled: true),
       });
+    });
+
+    test('include config using package: uri', () async {
+      final dir = createDir();
+      final file = createAnalysisOptions('''
+include: package:$testPackageName/analysis_options.yaml
+      ''');
+
+      final tempProjectDir = await createTempProject(dir.path, testPackageName);
+
+      final patchedPackageConfig = patchPackageConfig(
+        packageConfig,
+        testPackageName,
+        tempProjectDir,
+      );
+      final configs = CustomLintConfigs.parse(file, patchedPackageConfig);
+
+      expect(configs.rules.containsKey('from_package'), true);
+    });
+
+    test('if package: uri is not resolved default to empty', () async {
+      const notExistingFileName = 'this-does-not-exist';
+
+      final file = createAnalysisOptions('''
+include: package:$testPackageName/$notExistingFileName
+      ''');
+      final dir = createDir();
+
+      final tempProjectDir = await createTempProject(dir.path, testPackageName);
+      final patchedPackageConfig = patchPackageConfig(
+        packageConfig,
+        testPackageName,
+        tempProjectDir,
+      );
+      final configs = CustomLintConfigs.parse(file, patchedPackageConfig);
+
+      expect(configs, same(CustomLintConfigs.empty));
+    });
+
+    test('if package: uri is not valid default to empty', () async {
+      const notExistingPackage = 'this-package-does-not-exists';
+
+      final file = createAnalysisOptions('''
+include: package:$notExistingPackage/analysis_options.yaml
+      ''');
+      final dir = createDir();
+      final tempProjectDir = await createTempProject(dir.path, testPackageName);
+
+      final patchedPackageConfig = patchPackageConfig(
+        packageConfig,
+        testPackageName,
+        tempProjectDir,
+      );
+      final configs = CustomLintConfigs.parse(file, patchedPackageConfig);
+
+      expect(configs, same(CustomLintConfigs.empty));
     });
 
     test('if custom_lint.rules is present, merges with "include"', () {
@@ -168,7 +273,7 @@ custom_lint:
     foo: 21
   - d
 ''');
-      final configs = CustomLintConfigs.parse(analysisOptions);
+      final configs = CustomLintConfigs.parse(analysisOptions, packageConfig);
 
       expect(configs.enableAllLintRules, true);
       expect(configs.rules, {
@@ -185,11 +290,14 @@ custom_lint:
     group('Handles errors', () {
       test('Defaults to empty if yaml fails to parse', () {
         final configs = CustomLintConfigs.parse(
-          createAnalysisOptions('''
+          createAnalysisOptions(
+            '''
 foo:
     bar:
   baz:
-'''),
+''',
+          ),
+          packageConfig,
         );
         expect(configs, CustomLintConfigs.empty);
       });
