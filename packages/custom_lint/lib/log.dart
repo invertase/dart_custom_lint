bb857c7243fa1d2a36a93a33a2de140c54af1734
Remi Rousselet <darky12s@gmail.com>
2024-08-14 22:53:01 +0200

    Disable log

--- packages/custom_lint/lib/log.dart
+++ packages/custom_lint/lib/log.dart
@@ -4,7 +4,7 @@ final file = File('/Users/remirousselet/dev/rrousselGit/riverpod/log.txt');
 
 @Deprecated('message')
 void log(Object msg) {
-  file
-    ..createSync(recursive: true)
-    ..writeAsStringSync('$msg\n', mode: FileMode.append);
+  // file
+  //   ..createSync(recursive: true)
+  //   ..writeAsStringSync('$msg\n', mode: FileMode.append);
 }
